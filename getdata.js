58424e8ffef016a8596992142d4e267577dee0ec
aovoq <a21dc163@dhw.ac.jp>
2022-06-14 10:12:05 +0900

    getdata quarter argv support

--- getdata.js
+++ getdata.js
@@ -2,6 +2,24 @@ import puppeteer from 'puppeteer'
 import fs from 'fs'
 import 'dotenv/config'
 
+let quarter
+
+if (process.argv[2] !== undefined) {
+   if (process.argv[2].indexOf('-q') === 0) {
+      quarter = process.argv[2].split('=')[1]
+   } else {
+      console.log('please enter [quarter argv]')
+      console.log('sample: node getdata.js -q=1')
+      console.log('--- Get 1quarter timetable')
+      process.exit()
+   }
+} else {
+   console.log('please enter [quarter argv]')
+   console.log('sample: node getdata.js -q=1')
+   console.log('--- Get 1quarter timetable')
+   process.exit()
+}
+
 if (!process.env.DHU_ID || !process.env.DHU_PASS) {
    console.log('Please touch .env file.')
 } else {
@@ -21,13 +39,13 @@ if (!process.env.DHU_ID || !process.env.DHU_PASS) {
          await page.click('button#pmPage\\:j_idt32')
          await page.click('.ui-panel-inner > ul > li:nth-of-type(7) > a')
          await page.waitForNavigation()
-         const result = await page.evaluate(() => {
+         const result = await page.evaluate((quarter) => {
             const array = Array.from(
-               document.querySelector('table').querySelectorAll('tbody > tr'),
+               document.querySelector(`#pmPage\\:funcForm\\:j_idt87 > table:nth-of-type(${quarter})`).querySelectorAll('tbody > tr'),
                (row) => Array.from(row.querySelectorAll('th, td'), (cell) => cell.innerText),
             )
             return array
-         })
+         }, quarter)
          result.splice(2, 1)
          result.map((item) => item.shift())
          browser.close()
